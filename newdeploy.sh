1d07d91d57e8108b1e5828adc3560c569a5824c7
Divya9427 <59822480+Divya9427@users.noreply.github.com>
2020-02-10 16:46:22 +0530

    Update newdeploy.sh

--- newdeploy.sh
+++ newdeploy.sh
@@ -2,4 +2,4 @@ PID=`ps -ef | grep -v grep | grep java | awk '{print $2}'`
   
 kill -9 $PID
 
-nohup java -jar /root/spring-petclinic-2.2.0.BUILD-SNAPSHOT.jar &
+nohup java -jar /root/spring-petclinic-2.2.0.jar &
